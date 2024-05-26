3a28901eb036099335ad2857e422154ffebc01e6
dnannjk <nannajkardeepali@gmail.com>
2024-05-27 02:19:09 +0530

    file added

---
+++ src/main/java/com/myfirstdeployment/project/controllers/ProfileController.java
@@ -0,0 +1,19 @@
+package com.myfirstdeployment.project.controllers;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myfirstdeployment.project.data.User;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class ProfileController {
+
+    @GetMapping(value = "users", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> getUser() {
+        return List.of(new User("Pradeep", "Kolapkar", "pradeepkolpkar@gmail.com"), new User("Deepali", "Nannajkar", "deepali.nannjakar95@gmail.com"));
+    }
+}
